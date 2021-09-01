c8130f5de9815b10c980e825bf845416fd7d671d
Yzn Mohsen <yazanm008@gmail.com>
2021-09-01 07:57:19 +0300

    receiver_clinics

--- database/migrations/2021_07_31_165227_create_receiver_clinics_table.php
+++ database/migrations/2021_07_31_165227_create_receiver_clinics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class aCreateReceiverClinicsTable extends Migration
+class CreateReceiverClinicsTable extends Migration
 {
     /**
      * Run the migrations.
